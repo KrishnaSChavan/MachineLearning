9edb0ff51b34dd3377071228b843d72c78677f8f
Krishna <krishnachavan538@gmail.com>
2023-06-18 16:17:44 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,139 @@
+import zipfile
+import tensorflow_hub as hub
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimage
+import datetime
+import os
+import itertools
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+def extract_zip_file(File_name):
+    zip_ref = zipfile.ZipFile(File_name, "r")
+    zip_ref.extractall()
+    zip_ref.close()
+    
+def walkthrough(dir)
+    for dirpath, dirnames, filenames in os.walk("dir"):
+        print(f"There are {dirnames} directories and {len(filenames)} images in '{dirpath}'.")
+
+
+
+def view_random_img(target_dir,target_class):
+    #targeting a directory
+    target_folder = target_dir+"/"+target_class
+
+    # getting random image path
+    random_image = random.sample(os.listdir(target_folder),1)
+
+    #reading image and ploting
+    img = mpimg.imread(target_folder + "/" + random_image[0])
+    plt.imshow(img)
+    plt.title(target_class)
+    # show input shape
+    print(f"Image shape = {img.shape}")
+    return(img)
+    
+
+
+def get_model_details(model = None):
+    return tf.keras.utils.plot_model(model,to_file='model.png', show_shapes=True, show_dtype=True, show_layer_names=True, rankdir='TB', expand_nested=True, dpi=96, layer_range=None, show_layer_activations=True)
+     
+def loss_curve(history):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    
+    accuracy = history.history['accuracy']
+    val_accuracy = history.history['val_accuracy']
+    epochs =  range(len(history.history['loss']))
+
+    plt.plot(epochs,loss,label='loss')
+    plt.plot(epochs,val_loss,label='val_loss')
+    plt.title('loss')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs,accuracy,label='accuracy')
+    plt.plot(epochs,val_accuracy,label='val_accuracy')
+    plt.title('accuracy')
+    plt.legend()
+
+
+
+def callback_tb(dir_name,exp_name):
+    log_dir = dir_name+'/'+exp_name+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tb_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+    return tb_callback
+
+
+def create_model(model_url, num_classes):
+    # download model
+    feature_extraction_layer = hub.KerasLayer(model_url, trainable=False, name="feature_extraction", input_shape=IMAGE_SHAPE+(3,))
+    
+    # create our own model
+    model = Sequential()
+    model.add(feature_extraction_layer)
+    model.add(Dense(num_classes, activation='softmax', name='output'))
+    
+    return model
+
+
+
+
+# Our function needs a different name to sklearn's plot_confusion_matrix
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False):
+
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+  n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+  # Plot the figure and make it pretty
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+  fig.colorbar(cax)
+
+  # Are there a list of classes?
+  if classes:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+  
+  # Label the axes
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes), # create enough axis slots for each class
+         yticks=np.arange(n_classes),
+         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+         yticklabels=labels)
+  
+  # Make x-axis labels appear on bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  # Set the threshold for different colors
+  threshold = (cm.max() + cm.min()) / 2.
+
+  # Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Save the figure to the current working directory
+  if savefig:
+    fig.savefig("confusion_matrix.png")
+
+
+def walk_through_dir(dir_path):
+  for dirpath, dirnames, filenames in os.walk(dir_path):
+    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
